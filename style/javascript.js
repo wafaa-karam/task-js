85357c9887e94f1146bcdff1af21e6b12a34ad03
wafaakaram <wafaakaram877@gmail.com>
2022-05-10 22:41:31 +0300

    done task java script

---
+++ style/javascript.js
@@ -0,0 +1,5 @@
+window.onload=function() {
+    document.querySelector("h1").style.
+    color="red";
+}
+console.log("Hello %c JS %c Project",`color:blue; font-size:45px;`,`color:red; font-size:40px; background-color:blue; padding:5px; margin:6px;`)
